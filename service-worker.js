6a541de58ae4e88f97cdce32913b5bf9223334d7
samivolkan <samivolkann@gmail.com>
2025-07-11 15:52:30 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+
+self.addEventListener("install", function(e) {
+  console.log("Service Worker installed");
+});
+
+self.addEventListener("fetch", function(e) {
+  e.respondWith(fetch(e.request));
+});
